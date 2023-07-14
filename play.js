49afc3312a562af6b57f1e2ef69badb1c44df9eb
Alexander Evans <sander7989@gmail.com>
2023-07-13 18:47:21 -0600

    create seperate client file

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const net = require("net");
+const {connect} = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",// IP address here,
-    port: 50541// PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
