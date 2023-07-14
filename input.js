54605eb194c12af1fde31fe015886ef6bd583869
Alexander Evans <sander7989@gmail.com>
2023-07-13 19:25:00 -0600

    add movement input

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+  
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -6,6 +10,10 @@ const setupInput = function() {
   
   const handleUserInput = function(key) {
     key === "\u0003" ? process.exit() : null;//Quit if user enters control + C
+    key === "w" ? connection.write("Move: up") : null;
+    key === "a" ? connection.write("Move: left") : null;
+    key === "s" ? connection.write("Move: down") : null;
+    key === "d" ? connection.write("Move: right") : null;
   };
 
   stdin.on("data", handleUserInput);
