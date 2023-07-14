f3a1a6045e2200831e5789c98867808e588fb969
Alexander Evans <sander7989@gmail.com>
2023-07-13 19:11:48 -0600

    catch user input

--- play.js
+++ play.js
@@ -1,7 +1,21 @@
 const net = require("net");
 const {connect} = require("./client");
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+  const handleUserInput = function(key) {
+    key === "\u0003" ? process.exit() : null;//Quit if user enters control + C
+  };
 
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+};
 
 console.log("Connecting ...");
 connect();
+setupInput();
